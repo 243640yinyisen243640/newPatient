51c8215ec24189a6045e409e7f4cfa1441cfaf6d
username <email>
2023-02-03 15:11:27 +0800

    commit

--- app/src/main/java/com/vice/bloodpressure/ui/diet/DietChangeDietActivity.java
+++ app/src/main/java/com/vice/bloodpressure/ui/diet/DietChangeDietActivity.java
@@ -1,12 +1,15 @@
-package com.vice.bloodpressure.ui.diet;
+package com.vice.bloodpressure.ui.activity.diet;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.vice.bloodpressure.R;
 import com.vice.bloodpressure.baseui.UIBaseActivity;
+
 
 /**
  * 作者: beautiful
@@ -15,11 +18,52 @@ import com.vice.bloodpressure.baseui.UIBaseActivity;
  * 描述:
  */
 public class DietChangeDietActivity extends UIBaseActivity {
+    /**
+     * 早餐
+     */
+    private TextView breakfastTv;
+    /**
+     * 午餐
+     */
+    private TextView noonTv;
+    /**
+     * 晚餐
+     */
+    private TextView dinnerTv;
+    /**
+     * 早餐列表
+     */
+    private RecyclerView breakfastRv;
+    /**
+     * 午餐列表
+     */
+    private RecyclerView noonRv;
+    /**
+     * 晚餐列表
+     */
+    private RecyclerView dinnerRv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = View.inflate(getPageContext(), R.layout.activity_diet_change_diet_plan,null);
-        containerView().addView(view);
+        initView();
         topViewManager().titleTextView().setText(R.string.diet_activity_change_diet_top_title);
+        initValues();
+    }
+
+    private void initView() {
+        View view = View.inflate(getPageContext(), R.layout.activity_diet_change_diet_plan, null);
+        containerView().addView(view);
+        breakfastTv = view.findViewById(R.id.tv_diet_change_breakfast);
+        noonTv = view.findViewById(R.id.tv_diet_change_noon);
+        dinnerTv = view.findViewById(R.id.tv_diet_change_dinner);
+        breakfastRv = view.findViewById(R.id.rv_diet_change_breakfast);
+        noonRv = view.findViewById(R.id.rv_diet_change_noon);
+        dinnerRv = view.findViewById(R.id.rv_diet_change_dinner);
+    }
+
+    private void initValues() {
+
+
     }
 }
