cb2c9c0fcf0dc96ddd0f9ab7d951a7e1e99723e9
username <email>
2023-02-13 14:51:33 +0800

    commit

--- app/src/main/java/com/vice/bloodpressure/activity/ahome/adiet/DietMealPlanDetailsActivity.java
+++ app/src/main/java/com/vice/bloodpressure/activity/ahome/adiet/DietMealPlanDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.vice.bloodpressure.activity.ahome.adiet;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import com.vice.bloodpressure.R;
 import com.vice.bloodpressure.baseui.UIBaseLoadActivity;
 
 /**
@@ -17,8 +19,14 @@ public class DietMealPlanDetailsActivity extends UIBaseLoadActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        initViews();
     }
+
+    private void initViews() {
+        View view = View.inflate(getPageContext(), R.layout.activity_meal_plan_details, null);
+        containerView().addView(view);
+    }
+
 
     @Override
     protected void onPageLoad() {
