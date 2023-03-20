33e1d8ddb3616d02f5d35392bc38fcc3375462f9
username <email>
2023-03-20 13:06:16 +0800

    commit

--- app/src/main/java/com/vice/bloodpressure/activity/auser/UserIllImportantActivity.java
+++ app/src/main/java/com/vice/bloodpressure/activity/auser/UserIllImportantActivity.java
@@ -37,8 +37,12 @@ public class UserIllImportantActivity extends UIBaseLoadActivity {
     private CheckBox gaoCb;
     private ImageView arrowImageView;
     private LinearLayout bgLinearLayout;
+    private LinearLayout gaoLinearLayout;
     private HHAtMostGridView mostGridView;
+    private HHAtMostGridView gaoMostGridView;
+    private HHAtMostGridView levelMostGridView;
     private TextView timeTv;
+    private TextView gaoTimeTv;
     private TextView saveTv;
 
     @Override
@@ -64,41 +68,81 @@ public class UserIllImportantActivity extends UIBaseLoadActivity {
 
         PerfectDiseaseAdapter levelAdapter = new PerfectDiseaseAdapter(getPageContext(), list);
         mostGridView.setAdapter(levelAdapter);
+
+        List<UserInfo> diseaseList = new ArrayList<>();
+        UserInfo typeInfo1 = new UserInfo("1级高血压", "1");
+        diseaseList.add(typeInfo1);
+        UserInfo typeInfo2 = new UserInfo("2级高血压", "2");
+        diseaseList.add(typeInfo2);
+        UserInfo typeInfo3 = new UserInfo("3级高血压", "3");
+        diseaseList.add(typeInfo3);
+
+        PerfectDiseaseAdapter adapter = new PerfectDiseaseAdapter(getPageContext(), diseaseList);
+        gaoMostGridView.setAdapter(adapter);
+
+        List<UserInfo> levelList = new ArrayList<>();
+        UserInfo levelInfo1 = new UserInfo("低危", "1");
+        levelList.add(levelInfo1);
+        UserInfo levelInfo2 = new UserInfo("中危", "2");
+        levelList.add(levelInfo2);
+        UserInfo levelInfo3 = new UserInfo("高危", "3");
+        levelList.add(levelInfo3);
+        UserInfo levelInfo4 = new UserInfo("很高危", "4");
+        levelList.add(levelInfo4);
+
+        PerfectDiseaseAdapter gaoLevelAdapter = new PerfectDiseaseAdapter(getPageContext(), levelList);
+        levelMostGridView.setAdapter(gaoLevelAdapter);
     }
 
     private void initListener() {
         tangCb.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
+                bgLinearLayout.setVisibility(View.VISIBLE);
+                gaoLinearLayout.setVisibility(View.GONE);
                 gaoCb.setChecked(false);
-                setVisibility(View.GONE);
             } else {
+                bgLinearLayout.setVisibility(View.GONE);
+                gaoLinearLayout.setVisibility(View.VISIBLE);
+
                 gaoCb.setChecked(true);
-                setVisibility(View.VISIBLE);
             }
         });
         gaoCb.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
+                bgLinearLayout.setVisibility(View.GONE);
+                gaoLinearLayout.setVisibility(View.VISIBLE);
                 tangCb.setChecked(false);
-                setVisibility(View.GONE);
             } else {
+                bgLinearLayout.setVisibility(View.VISIBLE);
+                gaoLinearLayout.setVisibility(View.GONE);
                 tangCb.setChecked(true);
-                setVisibility(View.VISIBLE);
             }
         });
         timeTv.setOnClickListener(v -> {
-            PickerViewUtils.showTimeWindow(getPageContext(), new boolean[]{true, true, true, false, false, false}, DataFormatManager.TIME_FORMAT_Y_M_D, object -> {
-                timeTv.setText(object.toString());
-            });
+          showTime("1");
+        });
+        gaoTimeTv.setOnClickListener(v -> {
+            showTime("2");
         });
         saveTv.setOnClickListener(v -> {
 
         });
     }
 
-    private void setVisibility(int visibility) {
-        arrowImageView.setVisibility(visibility);
-        bgLinearLayout.setVisibility(visibility);
+    /**
+     *
+     * @param type 1:血糖 2：血压
+     */
+    private void showTime(String type){
+        PickerViewUtils.showTimeWindow(getPageContext(), new boolean[]{true, true, true, false, false, false}, DataFormatManager.TIME_FORMAT_Y_M_D, object -> {
+            if ("1".equals(type)){
+                timeTv.setText(object.toString());
+            }else {
+                gaoTimeTv.setText(object.toString());
+            }
+        });
     }
+
 
     private void initView() {
         View view = View.inflate(getPageContext(), R.layout.activity_user_ill_important, null);
@@ -106,8 +150,12 @@ public class UserIllImportantActivity extends UIBaseLoadActivity {
         gaoCb = view.findViewById(R.id.cb_user_ill_gao);
         arrowImageView = view.findViewById(R.id.iv_user_ill_important);
         bgLinearLayout = view.findViewById(R.id.ll_user_ill_important);
+        gaoLinearLayout = view.findViewById(R.id.ll_user_ill_important_gao);
         mostGridView = view.findViewById(R.id.gv_user_ill_important);
-        timeTv = view.findViewById(R.id.tv_user_ill_important_time);
+        gaoMostGridView = view.findViewById(R.id.gv_user_ill_important_gao_type);
+        levelMostGridView = view.findViewById(R.id.gv_user_ill_important_gao_level);
+        gaoTimeTv = view.findViewById(R.id.tv_user_ill_important_time);
+        timeTv = view.findViewById(R.id.tv_user_ill_important_gao_time);
         saveTv = view.findViewById(R.id.tv_user_ill_important_save);
         containerView().addView(view);
     }
